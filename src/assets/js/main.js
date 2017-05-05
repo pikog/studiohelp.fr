5ce6352dede357e8ba330d344b806dd96f7d57e7
Pikog <perso.pikog@gmail.com>
2017-05-05 09:48:35 +0200

    Add smoothstate.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,25 @@
-//=require include/_test.js
+$(function() {
 
-function main() {
+  var options = {
+    onStart: {
+      duration: 250, // Duration of our animation
+      render: function ($container) {
+        // Add your CSS animation reversing class
+        $container.addClass('is-exiting');
+      }
+    },
+    onReady: {
+      duration: 0,
+      render: function ($container, $newContent) {
+        console.log("prout");
+        $container.removeClass('is-exiting');
 
-}
+        // Inject the new content
+        $container.html($newContent);
+
+      }
+    }
+  };
+
+  $('#smoothStateContent').smoothState(options);
+});
